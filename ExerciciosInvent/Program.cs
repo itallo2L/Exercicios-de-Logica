8031f9dc2ae0e00ebb847d043640d5158981ec29
Paulo Sergio <paulosergio_gyn@hotmail.com>
2024-05-10 09:50:09 -0300

    Projeto adicionado ao repositorio

---
+++ ExerciciosInvent/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ExerciciosInvent
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
